fe6a1d204b1000e6483760cbec49ca2cac4296c8
amini <aminiamindi@gmail.com>
2023-07-12 17:30:11 +0330

    useradd coded

--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 import string
 import random
 from datetime import datetime
+import os
 
 
 def generate_user():
@@ -9,7 +10,14 @@ def generate_user():
     password = ''.join(random.choice(string.ascii_letters + string.digits + '!@#$%') for _ in range(14))
     generate_date = datetime.now()
     expired_time = generate_date.replace(month=generate_date.month + 1)
-    print(user_id)
+    return user_id, username, password, expired_time
 
 
 generate_user()
+
+
+def adduser(user_id, username, password, expired_time):
+    os.system(
+        f"useradd -m   --user-group  --comment  {user_id}  -d /home/{username}  -s /bin/true "
+        f"--password $(printf %s {password} | openssl passwd -1 -stdin) {username} ")
+    os.system(f"usermod -e  {expired_time.date()}   {username} ")
